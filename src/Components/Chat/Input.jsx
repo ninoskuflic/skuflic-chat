80ede72f15db8dc38b8c89c6a9917bbe9d331743
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-09-11 23:45:51 +0200

    Update

--- src/Components/Chat/Input.jsx
+++ src/Components/Chat/Input.jsx
@@ -1,4 +1,3 @@
-import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
 import { useState } from 'react';
 import React from 'react';
 
@@ -28,9 +27,6 @@ export default function Input(props) {
         <form onSubmit={e => onSubmit(e)}>
             <div className='input-group'>
                 <textarea onChange={e => onChange(e)} onKeyDown={(e) => handleKeyPress(e)} value={text} placeholder='Enter your message' autoFocus={true} rows='1' />
-                <span className='input-group-text' onClick={e => onSubmit(e)}>
-                    <span className='material-symbols-outlined'>add_reaction</span>
-                </span>
                 <span className='input-group-text' onClick={e => onSubmit(e)}>
                     <span className='material-symbols-outlined'>arrow_forward_ios</span>
                 </span>
