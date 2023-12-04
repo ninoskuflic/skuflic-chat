8bfd6ff458452b72fa45a714e70180da90761a3f
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-12-04 10:21:24 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
 
   useEffect(() => {
     if (chat.member.username !== '') {
-      const drone = new window.Scaledrone('ZeDWlA1xqWo7lYkq', {
+      const drone = new window.Scaledrone('QWznza3Q6ihljlhU', {
         data: chat.member,
       });
       setDrone(drone);
